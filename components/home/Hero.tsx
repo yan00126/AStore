396acb3fd4124834de9c7bed9c2950794fb291e5
yan fei <yan00126@algonquinlive.com>
2025-06-23 23:51:49 -0400

    set up hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,26 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+          repudiandae quisquam quasi sapiente sed corrupti provident optio modi
+          reprehenderit cumque obcaecati aperiam? Quos quaerat, hic incidunt
+          ullam dolorum ex soluta!
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 export default Hero;
