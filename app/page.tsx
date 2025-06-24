08f856c665fe67ebc8acb1c87d4e956b228fbbd8
yan fei <yan00126@algonquinlive.com>
2025-06-24 00:28:46 -0400

    add loading effect

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import React from "react";
 // import { Button } from "@/components/ui/button";
 import Hero from "@/components/home/Hero";
 import FeatureProducts from "@/components/home/FeaturedProducts";
+import { Suspense } from "react";
+import LoadingContainer from "@/components/global/LoadingContainer";
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <FeatureProducts />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeatureProducts />
+      </Suspense>
     </>
   );
 };
