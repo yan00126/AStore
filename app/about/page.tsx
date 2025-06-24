33fc2a2c10f3e31ee5cd9f6c064a0b5378db9b35
yan fei <yan00126@algonquinlive.com>
2025-06-24 00:16:06 -0400

    update page, remove prisma bit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,19 @@
-import db from "@/utils/db";
-import { log } from "console";
-
-async function AboutPage() {
-  const profile = await db.testProfile.create({
-    data: {
-      name: "random name",
-    },
-  });
-
-  const users = await db.testProfile.findMany();
-  console.log(profile);
-
+function AboutPage() {
   return (
-    <div>
-      {users.map((user) => {
-        return (
-          <h2 key={user.id} className="text-2xl font-bold">
-            {user.name}
-          </h2>
-        );
-      })}
-    </div>
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We Love
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          store
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero vero
+        laborum ullam! Sequi, veritatis et eveniet facilis optio voluptas unde
+        commodi sint, repellat consectetur soluta incidunt. Culpa nostrum id
+        fugiat.
+      </p>
+    </section>
   );
 }
 export default AboutPage;
