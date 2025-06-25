0bcfe2bbf54b16128495faeb79a3e55ee861c39c
yan fei <yan00126@algonquinlive.com>
2025-06-25 00:39:03 -0400

    ProductsContainer adding grid layout

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -1,6 +1,6 @@
 import ProductsGrid from "./ProductsGrid";
 import ProductsList from "./ProductList";
-import { LuLayoutGrid, LuList } from "react-icons/lu";
+import { LuLayoutGrid, LuLayoutList, LuList } from "react-icons/lu";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
@@ -19,7 +19,34 @@ async function ProductsContainer({
   return (
     <>
       {/* HEADER */}
-      <section></section>
+      <section>
+        <div className="flex justify-between items-center">
+          <h4 className="font-medium text-lg">
+            {totalProducts} product{totalProducts > 1 && "s"}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=grid${searchTerm}`}>
+                <LuLayoutGrid />
+              </Link>
+            </Button>
+            <Button
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=list${searchTerm}`}>
+                <LuList />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <Separator className="mt-4 " />
+      </section>
       {/* PRODUCTS */}
       <div>
         {totalProducts === 0 ? (
