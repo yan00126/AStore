7a8593d0e05f285eb7bd9e2391c18cd44d1d42bf
yan fei <yan00126@algonquinlive.com>
2025-08-13 20:56:07 -0400

    add toast for  features to add in the future

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { ThemeProvider } from "./theme-provider";
-
+import { Toaster } from "sonner";
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
