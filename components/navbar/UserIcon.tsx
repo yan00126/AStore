bea8008333a6287901de7308b2ea0210994b291c
yan fei <yan00126@algonquinlive.com>
2025-08-13 23:33:17 -0400

    UserIcon defined

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,20 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { LuUser } from "react-icons/lu";
+
+async function UserIcon() {
+  // const { userId } = auth();
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if (profileImage) {
+    return (
+      <Image
+        alt="profileImage"
+        src={profileImage}
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
