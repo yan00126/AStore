c8bdcc20bac7f72835db0fd5719cbe389892b60d
yan fei <yan00126@algonquinlive.com>
2025-08-16 01:33:34 -0400

    add create product details and faker installled

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,4 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { faker } from "@faker-js/faker";
+const CreateProductAction = async (formData: FormData) => {
+  "use server";
+  const name = formData.get("name") as string;
+  console.log(name);
+};
+
 function CreateProductPage() {
-  return <div>CreateProductPage</div>;
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 20 });
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">create product</h1>
+      <div className="border p-8 rounded-md">
+        <form action={CreateProductAction}>
+          <div className="mb-2">
+            <Label htmlFor="name">Product Name</Label>
+            <Input id="name" name="name" type="text" defaultValue={name} />
+          </div>
+          <Button type="submit" size="lg">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
 export default CreateProductPage;
